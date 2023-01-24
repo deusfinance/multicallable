5f0a2bfb5e5939aa80bf0e681198692936118111
Hassan Abbasi <hassan.abbp@gmail.com>
2023-01-24 17:58:14 +0330

    Update copyright date

--- src/multicallable/__init__.py
+++ src/multicallable/__init__.py
@@ -1,7 +1,7 @@
 # Multicallable - Easy way to work with Multicall package
 
 __author__ = "Hassan Abbasi, Mojtaba Farokhi"
-__copyright__ = "Copyright (C) 2022, Deus Finance <https://github.com/deusfinance>"
+__copyright__ = "Copyright (C) 2022-2023, Deus Finance <https://github.com/deusfinance>"
 __credits__ = ["Hassan Abbasi", "Mojtaba Farokhi"]
 __license__ = "MIT"
 __version__ = "4.0.0"
