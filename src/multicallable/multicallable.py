eaa3aefcd65f66128a669098652fb8f202d9e5d2
Hassan Abbasi <hassan.abbp@gmail.com>
2023-02-28 16:06:17 +0330

    Add progress bar

--- src/multicallable/multicallable.py
+++ src/multicallable/multicallable.py
@@ -3,6 +3,24 @@ from web3 import Web3
 from .multicall import Multicall, Call
 
 
+def bar(percentage: float, size: int = 40):
+    percentage = int(percentage)
+    hori_char = '━'
+    left_char = '╸'
+    pink = (249, 38, 114)
+    grey = (58, 58, 58)
+    green = (114, 156, 31)
+    start_pink = '\033[38;2;%d;%d;%dm' % pink
+    start_grey = '\033[38;2;%d;%d;%dm' % grey
+    start_green = '\033[38;2;%d;%d;%dm' % green
+    reset_color = '\033[39m'
+    if percentage >= 100:
+        return f'{start_green}{hori_char * size}{reset_color}'
+    filled = hori_char * (size * percentage // 100)
+    not_filled = hori_char * (size - len(filled) - 1)
+    return f'{start_pink}{filled}{left_char}{start_grey}{not_filled}{reset_color}'
+
+
 def _split(a, n):
     k, m = divmod(len(a), n)
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
@@ -15,15 +33,20 @@ class Multicallable:
                 self.function = function
                 self.params = params
 
-            def call(self, n: int = 1, require_success: bool = True):
+            def call(self, n: int = 1, require_success: bool = True, progress_bar: bool = False):
                 mc = self.function.parent._multicall
                 calls = [Call(self.function.parent._target, self.function.name, args) for args in self.params]
                 result = []
-                for bucket in _split(calls, n):
+                for i, bucket in enumerate(_split(calls, n)):
+                    if progress_bar:
+                        percentage = i / n * 100
+                        print(f'\r    {bar(percentage)} {i}/{n} buckets    ', end='')
                     if not bucket:
                         continue
                     block_number, block_hash, outputs = mc.call(bucket, require_success=require_success)
                     result.extend(outputs)
+                if progress_bar:
+                    print(f'\r    {bar(100)} {n}/{n} buckets    ')
                 return result
 
             def detailed_call(self, n: int = 1, require_success: bool = True):
