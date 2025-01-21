990c8ced25aef5615f7fffec42ce9aaf3ecb62ed
MiKO <39623615+MiKoronjoo@users.noreply.github.com>
2025-01-21 11:42:12 +0330

    Update version | Update copyright

--- src/multicallable/__init__.py
+++ src/multicallable/__init__.py
@@ -1,10 +1,10 @@
 # Multicallable - Easy way to work with Multicall package
 
 __author__ = "MiKO, Naveed"
-__copyright__ = "Copyright (C) 2022-2024, Deus Finance <https://github.com/deusfinance>"
+__copyright__ = "Copyright (C) 2022-2025, Deus Finance <https://github.com/deusfinance>"
 __credits__ = ["MiKO", "Naveed"]
 __license__ = "MIT"
-__version__ = "7.1.0"
+__version__ = "7.2.0"
 __maintainer__ = "MiKO"
 __email__ = "mikoronjoo@gmail.com, naveedinno@proton.me"
 __status__ = "Production"
